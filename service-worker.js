eb1822876f3486ff8b21b5a32e56e9aa3462583e
pritymistry <77926278+pritymistry@users.noreply.github.com>
2024-07-08 00:03:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,18 @@
 const CACHE_NAME = 'news-pwa-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/styles.css',
-    '/js/main.js',
-    '/manifest.json',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-512x512.png'
+    './',
+    './index.html',
+    './css/styles.css',
+    './js/main.js',
+    './manifest.json',
+    './data.json',
+    './images/icons/icon-72x72.png',
+    './images/icons/icon-96x96.png',
+    './images/icons/icon-128x128.png',
+    './images/icons/icon-144x144.png',
+    './images/icons/icon-152x152.png',
+    './images/icons/icon-192x192.png',
+    './images/icons/icon-512x512.png'
 ];
 self.addEventListener('fetch', event => {
     const url = 'data.json';
