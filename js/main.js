c7c2684f2e9f670526d62fa38622e7f919a9bc82
pritymistry <77926278+pritymistry@users.noreply.github.com>
2024-07-07 15:13:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function async() {
 
   fetch(
-    "../data.json"
+    "data.json"
   )
     .then((response) => response.json())
     .then((data) => {
